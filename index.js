cc8697253ca19f27932febbfc80babdbac082afc
Leslie <wilson.leslie2021@gmail.com>
2025-10-19 18:32:28 -0400

    Adding schema

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import 'dotenv/config'
+import express from 'express';
 
-const app = express()
+import mongoose from 'mongoose';
 
+const app = express();
+const port = process.env.PORT || 3000;
 
+await mongoose.connect (process.env.ATLAS_URI);
+
+app.get('/', (req,res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, async () => {
+    console.log('listening:', port)
+});
 
 
 
